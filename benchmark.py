4bf0f1735b45ac46489a3a5606aa0478d9e3fabc
Adam Wawrzynski <adamwawrzynski@wp.pl>
2020-01-03 12:45:45 +0100

    benchmark.py: change evaluation function, adjust code changes in different files
    Signed-off-by: Adam Wawrzynski <adamwawrzynski@wp.pl>

--- benchmark.py
+++ benchmark.py
@@ -5,23 +5,25 @@ import pandas as pd
 import numpy as np
 import time
 from sklearn.model_selection import train_test_split
-from sklearn.datasets import load_files
 from sklearn.model_selection import KFold
 
+from load_dataset import load_bbc_dataset
+from load_dataset import load_news_groups_dataset
+from load_dataset import load_yahoo_answers_dataset
+from load_dataset import load_ohsumed_dataset
+from load_dataset import load_reuters_dataset
 from tfidf_model import TfIdfModel
 from doc2vec_model import Doc2VecDBOWModel, Doc2VecDMModel
 from lda_model import LDAModel
 from lsa_model import LSAModel
 from han import HAN
 from han_model import HANModel
+from sif_model import SIFModel
 from cbow_model import CBOWModel
 
 import logging
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
-    level=logging.INFO)
-
 import multiprocessing
-cores = multiprocessing.cpu_count()-2
+cores = multiprocessing.cpu_count()-1
 
 
 def cross_validation(
@@ -34,13 +36,17 @@ def cross_validation(
 
     for model in benchmark_models:
         scores = []
-        for train_text, train_target in cv.split(train['text'], y_train['target']):
+        trainig_times = []
+        for train_text, train_target in cv.split(train, y_train):
             model.build_model()
-            model.train(train['text'][train_text], y_train['target'][train_text])
-            model.fit(train['text'][train_text], y_train['target'][train_text])
-            scores.append(model.evaluate(train['text'][train_target], y_train['target'][train_target]))
+            t0 = time.time()
+            model.train(train[train_text], y_train[train_text])
+            trainig_times.append((time.time() - t0))
+            model.fit(train[train_text], y_train[train_text])
+            scores.append(model.evaluate(train[train_target], y_train[train_target]))
 
-        print("Average: ", np.average(scores))
+        logging.info(model.__class__.__name__ + ": average training time: " + str(np.average(np.array(trainig_times))))
+        logging.info(model.__class__.__name__ + ": average score: " + str(np.average(scores)))
 
 
 parser =  argparse.ArgumentParser(
@@ -68,87 +74,101 @@ parser.add_argument("-r",
     "--restore",
     dest="restore",
     required=False,
-    default=False,
-    type=bool,
+    action='store_true',
     help="Path to models")
+
+parser.add_argument("-l",
+    "--logging",
+    dest="logging",
+    required=False,
+    help="Path to logging file")
 
 args = parser.parse_args()
 
-data = load_files(args.dataset,
-    encoding="us-ascii",
-    decode_error="ignore")
-dataset_y = pd.DataFrame(data.target, columns=['target'])
-dataset = pd.DataFrame(data.data, columns=['text'])
+if args.logging:
+    logging.basicConfig(
+    filename=args.logging,
+    format='%(asctime)s : %(levelname)s : %(message)s',
+    level=logging.INFO)
+else:
+    logging.basicConfig(
+    format='%(asctime)s : %(levelname)s : %(message)s',
+    level=logging.INFO)
+
+logging.info(args.dataset)
+
+# x, y = load_bbc_dataset(args.dataset)
+x, y = load_yahoo_answers_dataset(args.dataset)
+# x, y = load_news_groups_dataset(args.dataset)
+# x, y = load_reuters_dataset(args.dataset)
+# x, y = load_ohsumed_dataset(args.dataset)
 
 train, test, y_train, y_test = train_test_split(
-    dataset,
-    dataset_y,
+    x,
+    y,
     train_size=0.2)
 
 train = train.reset_index(drop=True)
 y_train = y_train.reset_index(drop=True)
 
-benchmark_models = []
-
 han = HANModel(
     text = train['text'],
     labels = y_train['target'],
-    num_categories = 5,
+    num_categories = 10,
     pretrained_embedded_vector_path = args.pretrained_path,
     max_features = 200000,
-    max_senten_len = 150,
-    max_senten_num = 20,
+    max_senten_len = 100,
+    max_senten_num = 30,
     embedding_size = 100,
     validation_split=0.2,
     verbose=1,
-    epochs=1)
+    batch_size=8,
+    epochs=10)
 
 doc2vecdm = Doc2VecDMModel(
     negative=10,
     vector_size=100,
     window=5,
     workers=cores,
-    min_count=2)
+    min_count=1)
 
 doc2veccbow = Doc2VecDBOWModel(
     negative=10,
     vector_size=100,
     window=5,
     workers=cores,
-    min_count=2)
+    min_count=1)
+
+sif = SIFModel(
+    text = train['text'],
+    labels = y_train['target'],
+    pretrained_embedded_vector_path = args.pretrained_path,
+    embedding_size=100)
 
 lda = LDAModel(
-    n_components=10,
-    max_df=0.75,
-    min_df=7,
-    epochs=100,
+    n_components=100,
+    max_features=None,
+    max_df=0.95,
+    min_df=1,
+    epochs=10,
     cores=cores)
 
 lsa = LSAModel(
-    svd_features=10,
-    n_features=2000, 
-    n_iter=40,
-    max_df=0.75,
-    min_df=7)
+    svd_features=100,
+    n_features=None,
+    n_iter=10,
+    max_df=0.95,
+    min_df=1)
 
 tfidf = TfIdfModel(
-    n_features=2000,
-    max_df=0.75,
-    min_df=7)
+    n_features=None,
+    max_df=0.95,
+    min_df=1)
 
 cbow = CBOWModel(
-    max_df=0.75,
-    min_df=7)
+    max_features=None,
+    max_df=0.95,
+    min_df=1)
 
-benchmark_models = [han, doc2vecdm, doc2veccbow, lda, lsa, tfidf]
-
-for model in benchmark_models:
-    if not args.restore or not model.can_load(args.models_path):
-        model.train(train['text'], y_train['target'])
-        model.fit(train['text'], y_train['target'])
-
-        model.save(args.models_path)
-    else:
-        model.load(args.models_path)
-
-    model.evaluate(test['text'], y_test['target'])
+benchmark_models = [cbow, tfidf, lsa, lda, sif, doc2vecdm, doc2veccbow, han]
+cross_validation(benchmark_models, x['text'], y['target'], n_splits=5)
